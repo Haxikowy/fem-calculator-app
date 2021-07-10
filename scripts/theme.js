b0ee94c2c9da522ad390b2e09c4c9f094c7ac6a1
Szymon Dolnik <szymon.dolnik@gmail.com>
2021-07-10 14:18:17 +0200

    Add switch theme feature

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,7 +1,18 @@
 const container = document.querySelector('.container');
+const themeSelector = document.querySelector('.themeselector')
 
-const changeTheme = () => {
-  if (container.classList.contains('.theme-first')) {
-    console.log('FIREEEE');
+
+themeSelector.addEventListener('change', e => {
+  const theme = e.target.value
+
+  changeTheme(theme)
+})
+
+const changeTheme = (theme) => {
+  const lastTheme = container.classList[1];
+
+  if (!container.classList.contains(theme)) {
+    container.classList.remove(lastTheme)
+    container.classList.add(theme)
   }
 }
