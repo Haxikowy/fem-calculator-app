db156c39073a2cbf7da2242b7ecc72bcb28d3574
Szymon Dolnik <szymon2407@hotmail.com>
2021-07-10 18:43:43 +0200

    Add desktop layout, edited README

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,10 +1,10 @@
-const container = document.querySelector('.container');
+const body = document.querySelector('body');
 
 const changeTheme = (theme) => {
-  const lastTheme = container.classList[1];
+  const lastTheme = body.classList[0];
 
-  if (!container.classList.contains(theme)) {
-    container.classList.remove(lastTheme)
-    container.classList.add(theme)
+  if (!body.classList.contains(theme)) {
+    body.classList.remove(lastTheme)
+    body.classList.add(theme)
   }
 }
